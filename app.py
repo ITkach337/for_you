19e167c2ce8a4f0acd2981c986884e1b2c33b4c5
ITkach337 <79201068+ITkach337@users.noreply.github.com>
2022-08-02 12:39:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,3 @@ app = Flask(__name__)
 def index():
 <<<<<<< HEAD
     return '<h1>Hello men img="1.jpg"</h1>'
-=======
-    return '<h1>Hello WORLD</h1>'
->>>>>>> 82a0ef5f520b60066417b3d7d0efb73ac3b52ebb
